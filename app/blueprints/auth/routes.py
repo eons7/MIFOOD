03a2fea8c1af9b0acdb6c2d54b6d1664a3dc4dae
eons7 <eon285764@gmail.com>
2026-03-26 21:50:16 +0300

    deдуеу route

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -1,13 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request
-from flask_login import login_user, logout_user, login_required, current_user
-from app import db
-from app.models import User
 
-auth_bp = Blueprint('auth', __name__)
-
-@auth_bp.route('/login', methods=['GET', 'POST'])
-def login():
-    return render_template("auth/login.html")
 
 
 
