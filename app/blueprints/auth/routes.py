5a6160956b59cea0a064f4f5393e4f00bc6e4826
eons7 <eon285764@gmail.com>
2026-03-26 21:41:01 +0300

    default route

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -1,5 +1,9 @@
-@routes.route("/login",)
+@auth_bp.route("/login",)
 def login():
     return render_template("auth/login.html")
+@auth_bp.route("/register",)
+def register():
+    return render_template("auth/register.html")
+
 
 
