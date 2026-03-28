763325cdb22e5a204ba7ecbb65a3305eb46370bf
eons7 <eon285764@gmail.com>
2026-03-28 15:50:08 +0300

    docs

--- app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Импортируй модели здесь после того, как создашь их файлы
-# from .user        import User
-# from .menu        import MenuItem, Category
-# from .order       import Order, OrderItem
-# from .reservation import Reservation, Table
